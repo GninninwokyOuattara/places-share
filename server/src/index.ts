cd4ad8aaabff2cb4e09ca53182a4bd1ef0aaae23
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-04-26 15:43:25 +0000

    error handling middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,35 @@
-// server
+import express from "express";
+import cors from "cors";
+import placesRoutes from "./routes/places-routes";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use("/api/places/", placesRoutes);
+
+// Error handling middleware
+app.use(
+    (
+        error: any,
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) => {
+        if (res.headersSent) {
+            return next(error);
+        }
+        res.status(error.code || 500);
+        res.json({ message: error.message });
+    }
+);
+
+app.get("*", async (req, res) => {
+    res.json({ status: "Running" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
