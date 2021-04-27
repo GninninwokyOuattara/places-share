fbc6c3d9d38048cd4ec0f4e1e47af7af8f9e05fc
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-04-27 00:45:46 +0000

    Add controllers for each place route

--- server/src/routes/places-routes.ts
+++ server/src/routes/places-routes.ts
@@ -1,83 +1,26 @@
-// const express = require("express")
 import express from "express";
-import HttpError from "../models/http-error";
+import {
+    getPlaceByPlaceId,
+    getPlacesByUserId,
+    getAllPlaces,
+    deletePlaceById,
+    postPlace,
+    updatePlaceById,
+} from "../controllers/places-controllers";
+// import HttpError from "../models/http-error";
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: "p1",
-        title: "The title",
-        description: "The places description",
-        location: {
-            lat: 40.74,
-            long: -73.44,
-        },
-        address: "20 W 34",
-        creator: "u1",
-    },
-    {
-        id: "p2",
-        title: "The title",
-        description: "The places description",
-        location: {
-            lat: 40.74,
-            long: -73.44,
-        },
-        address: "20 W 34",
-        creator: "u2",
-    },
-    {
-        id: "p3",
-        title: "The title",
-        description: "The places description",
-        location: {
-            lat: 40.74,
-            long: -73.44,
-        },
-        address: "20 W 34",
-        creator: "u1",
-    },
-    {
-        id: "p4",
-        title: "The title",
-        description: "The places description",
-        location: {
-            lat: 40.74,
-            long: -73.44,
-        },
-        address: "20 W 34",
-        creator: "u2",
-    },
-];
+router.get("/", getAllPlaces);
 
-router.get("/", (req, res) => {
-    res.json({ response: "Index Places route" });
-});
+router.get("/:pid", getPlaceByPlaceId);
 
-router.get("/:pid", async (req, res, next) => {
-    const place = DUMMY_PLACES.find((place) => place.id == req.params.pid);
+router.get("/user/:uid", getPlacesByUserId);
 
-    if (!place) {
-        return next(
-            new HttpError("No place was found for the provided place id", 404)
-        );
-    }
+router.post("/", postPlace);
 
-    return res.json({ place });
-});
+router.patch("/:pid", updatePlaceById);
 
-router.get("/user/:uid", async (req, res, next) => {
-    const user_places = DUMMY_PLACES.filter(
-        (place) => place.creator == req.params.uid
-    );
-    if (!user_places) {
-        return next(
-            new HttpError("No place was found for the provided user id", 404)
-        );
-    }
-
-    return res.json({ user_places });
-});
+router.delete("/:pid", deletePlaceById);
 
 export default router;
