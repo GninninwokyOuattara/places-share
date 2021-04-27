e4a591721acda4fa2c27d962f556604e5c948de0
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-04-27 22:36:21 +0000

    Location model for generating address and coordinate based on place

---
+++ server/src/models/location.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosResponse } from "axios";
+
+interface SuccessGeometry {
+    features: {
+        place_name: string;
+        geometry: {
+            coordinates: [number, number];
+        };
+    }[];
+}
+
+export class Location {
+    address: string;
+    error: boolean;
+    location!: {
+        lat: number | null;
+        long: number | null;
+    };
+    constructor(address: string) {
+        this.location = {
+            lat: null,
+            long: null,
+        };
+        this.address = address;
+        this.error = false;
+        (async () => this.getGeometry())();
+    }
+
+    getLocation() {
+        return {
+            location: this.location as { lat: number; long: number },
+            address: this.address,
+        };
+    }
+
+    async getGeometry() {
+        let queryUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(
+            this.address
+        )}.json?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`;
+
+        let response: AxiosResponse<SuccessGeometry> = await axios.get(
+            queryUrl
+        );
+        if (response.data.features.length == 0) {
+            this.error = true;
+        } else {
+            this.address = response.data.features[0].place_name;
+            this.location.lat =
+                response.data.features[0].geometry.coordinates[0];
+            this.location.long =
+                response.data.features[0].geometry.coordinates[1];
+        }
+    }
+}
