31dcbbd27c8927b39b6ae9a5af49295d3101c809
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-04-27 16:17:47 +0000

    basic validation middleware for users routes

--- server/src/controllers/users-controllers.ts
+++ server/src/controllers/users-controllers.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
 import HttpError from "../models/http-error";
 
 type Controllers = (req: Request, res: Response, next: NextFunction) => void;
@@ -41,14 +42,31 @@ export const getUsers: Controllers = async (req, res, next) => {
 };
 
 export const signUp: Controllers = async (req, res, next) => {
-    if (!Object.keys(req.body).length) {
-        return next(new HttpError("No body provided", 500));
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return next(
+            new HttpError("Invalid inputs passed, please check your data.", 422)
+        );
     }
+
+    let index = DUMMY_USERS.findIndex((user) => user.email == req.body.email);
+
+    if (index != -1) {
+        return next(new HttpError("Email already in use", 500));
+    }
+
     DUMMY_USERS = [...DUMMY_USERS, req.body];
     return res.json({ DUMMY_USERS });
 };
 
 export const logIn: Controllers = async (req, res, next) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return next(
+            new HttpError("Invalid inputs passed, please check your data.", 422)
+        );
+    }
+
     const { email, password } = req.body;
 
     let user = DUMMY_USERS.find(
--- server/src/routes/users-routes.ts
+++ server/src/routes/users-routes.ts
@@ -1,12 +1,27 @@
 import express from "express";
 import { getUsers, logIn, signUp } from "../controllers/users-controllers";
+import { check } from "express-validator";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 
-router.post("/signup", signUp);
+router.post(
+    "/signup",
+    [
+        check("email").isEmail(),
+        check("password").not().isEmpty().isLength({ min: 6 }),
+    ],
+    signUp
+);
 
-router.post("/login", logIn);
+router.post(
+    "/login",
+    [
+        check("email").isEmail(),
+        check("password").not().isEmpty().isLength({ min: 6 }),
+    ],
+    logIn
+);
 
 export default router;
