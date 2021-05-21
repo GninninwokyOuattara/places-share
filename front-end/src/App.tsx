105f45efabafc4def9d74c42bfcb72e14101c896
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-05-21 16:34:30 +0000

    dynamic route dependant on user login status & redirections

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import {
@@ -9,19 +9,19 @@ import {
 } from "react-router-dom";
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
-import UdatePlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./users/pages/Auth";
-import AuthProvider from "./shared/context/auth-context";
+import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
+    const auth = useContext(AuthContext) as { isLoggedIn: boolean };
     return (
-        <AuthProvider>
-            <Router>
-                <MainNavigation />
-                <main>
+        <Router>
+            <MainNavigation />
+            <main>
+                {auth.isLoggedIn ? (
                     <Switch>
                         <Route path="/" exact>
                             <Users />
@@ -35,14 +35,24 @@ function App() {
                         <Route path="/places/:placeid" exact>
                             <UpdatePlace />
                         </Route>
+                        <Redirect to="/" />
+                    </Switch>
+                ) : (
+                    <Switch>
+                        <Route path="/" exact>
+                            <Users />
+                        </Route>
+                        <Route path="/:uid/places" exact>
+                            <UserPlaces />
+                        </Route>
                         <Route path="/auth" exact>
                             <Auth />
                         </Route>
-                        <Redirect to="/" />
+                        <Redirect to="/auth" />
                     </Switch>
-                </main>
-            </Router>
-        </AuthProvider>
+                )}
+            </main>
+        </Router>
     );
 }
 export default App;
