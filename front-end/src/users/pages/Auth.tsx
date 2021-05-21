f3de4d0e28cecaf4c8396f04031e199de8b5b06c
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-05-21 14:09:41 +0000

    style auth page with course style sheet

--- front-end/src/users/pages/Auth.tsx
+++ front-end/src/users/pages/Auth.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./Auth.css";
+import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import {
@@ -28,29 +29,33 @@ const Auth = () => {
     };
 
     return (
-        <form action="" className="auth-form" onSubmit={loginHandler}>
-            <Input
-                id="email"
-                element="input"
-                type="text"
-                label="Email"
-                validators={[VALIDATOR_EMAIL()]}
-                errorText="Please enter a valid email address"
-                onInput={inputHandler}
-            />
-            <Input
-                id="password"
-                element="input"
-                type="text"
-                label="Password"
-                validators={[VALIDATOR_MINLENGTH(8)]}
-                errorText="Password must be at least 8 characters long"
-                onInput={inputHandler}
-            />
-            <Button type="submit" disabled={!formState.isValid}>
-                LOG IN
-            </Button>
-        </form>
+        <Card className="authentication">
+            <h2>Login Required</h2>
+            <hr />
+            <form action="" onSubmit={loginHandler}>
+                <Input
+                    id="email"
+                    element="input"
+                    type="text"
+                    label="Email"
+                    validators={[VALIDATOR_EMAIL()]}
+                    errorText="Please enter a valid email address"
+                    onInput={inputHandler}
+                />
+                <Input
+                    id="password"
+                    element="input"
+                    type="text"
+                    label="Password"
+                    validators={[VALIDATOR_MINLENGTH(8)]}
+                    errorText="Password must be at least 8 characters long"
+                    onInput={inputHandler}
+                />
+                <Button type="submit" disabled={!formState.isValid}>
+                    LOG IN
+                </Button>
+            </form>
+        </Card>
     );
 };
 
