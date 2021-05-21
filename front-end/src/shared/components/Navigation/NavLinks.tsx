46bb25e70de16cdf2d751b2b05f61f182da8ae00
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-05-21 15:52:46 +0000

    certains link appearing only for logged in users

--- front-end/src/shared/components/Navigation/NavLinks.tsx
+++ front-end/src/shared/components/Navigation/NavLinks.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
 import "./NavLinks.css";
+import { AuthContext } from "../../context/auth-context";
 
 const NavLinks: React.FC = () => {
+    const auth = useContext(AuthContext) as { isLoggedIn: boolean };
+
     return (
         <ul className="nav-links">
             <li>
@@ -11,12 +14,16 @@ const NavLinks: React.FC = () => {
                     ALL USERS
                 </NavLink>
             </li>
-            <li>
-                <NavLink to="/u1/places">MY PLACES</NavLink>
-            </li>
-            <li>
-                <NavLink to="/places/new">NEW PLACE</NavLink>
-            </li>
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/u1/places">MY PLACES</NavLink>
+                </li>
+            )}
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/places/new">NEW PLACE</NavLink>
+                </li>
+            )}
             <li>
                 <NavLink to="/auth">AUTHENTIFICATE</NavLink>
             </li>
