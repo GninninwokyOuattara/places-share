323698bafa206611a05b79bebb356f23d8145bbf
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-05-22 23:10:44 +0000

    change to schema to include default image

--- server/src/database/schema/user.ts
+++ server/src/database/schema/user.ts
@@ -17,7 +17,8 @@ const UserSchema = new Schema({
     },
     image: {
         type: String,
-        required: true,
+        default:
+            "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png",
     },
 
     places: [
