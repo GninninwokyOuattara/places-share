e64dd8fe187befda85b8e5abfe3969a96a26409a
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-05-22 00:27:54 +0000

    removed console.log

--- front-end/src/places/pages/UserPlaces.tsx
+++ front-end/src/places/pages/UserPlaces.tsx
@@ -8,8 +8,7 @@ const DUMMY_PLACES = [
         id: "p1",
         title: "empire state building",
         description: "one of the most famous place in the world",
-        image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/260px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/260px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
         address: "20 W 34 St, New York, NY 10001",
         location: {
             lat: 40.74,
@@ -21,8 +20,7 @@ const DUMMY_PLACES = [
         id: "p2",
         title: "empire state builde",
         description: "one of the most famous place in the world",
-        image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/260px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/260px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
         address: "20 W 34 St, New York, NY 10001",
         location: {
             lat: 40.74,
@@ -34,7 +32,6 @@ const DUMMY_PLACES = [
 
 const UserPlaces: React.FC = () => {
     const { uid } = useParams<{ uid: string }>();
-    console.log(uid);
     const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === uid);
     return <PlaceList items={loadedPlaces} />;
 };
