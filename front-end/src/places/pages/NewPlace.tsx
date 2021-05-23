4a22ee71357058c8ca00120358fe90be7e2c7955
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-05-23 17:18:33 +0000

    post places to db then redirect to home page

--- front-end/src/places/pages/NewPlace.tsx
+++ front-end/src/places/pages/NewPlace.tsx
@@ -1,4 +1,5 @@
-import React, { useCallback, useReducer } from "react";
+import React, { useCallback, useReducer, useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
@@ -6,11 +7,17 @@ import {
     VALIDATOR_MINLENGTH,
     VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import "./PlaceForm.css";
 
 import useForm from "../../shared/hooks/form-hook";
+import useHttpClient from "../../shared/hooks/http-hook";
+import { AuthContext } from "../../shared/context/auth-context";
 
 const NewPlace: React.FC = () => {
+    const { userId } = useContext(AuthContext) as { userId: string };
+    const [isLoading, error, sendRequest, clearError] = useHttpClient();
     const [formState, inputHandler] = useForm(
         {
             title: {
@@ -25,46 +32,68 @@ const NewPlace: React.FC = () => {
         false
     );
 
-    const formSubmitHandler: React.FormEventHandler<HTMLFormElement> = (
+    const history = useHistory();
+
+    const formSubmitHandler: React.FormEventHandler<HTMLFormElement> = async (
         event
     ) => {
         event.preventDefault();
-        console.log(formState.inputs);
+        try {
+            await sendRequest(
+                "http://localhost:5000/api/places/",
+                "POST",
+                JSON.stringify({
+                    title: formState.inputs.title.value,
+                    description: formState.inputs.description.value,
+                    address: formState.inputs.address.value,
+                    creator: userId,
+                }),
+                {
+                    "Content-Type": "application/json",
+                }
+            );
+
+            history.push("/");
+        } catch (error) {}
     };
 
     return (
-        <form action="" className="place-form" onSubmit={formSubmitHandler}>
-            <Input
-                id="title"
-                element="input"
-                type="text"
-                label="Title"
-                validators={[VALIDATOR_REQUIRE()]}
-                errorText="Please enter a valid title"
-                onInput={inputHandler}
-            />
-            <Input
-                id="description"
-                element="textarea"
-                type="text"
-                label="Description"
-                validators={[VALIDATOR_MINLENGTH(5)]}
-                errorText="Please enter a valid title (at least 5 characters)"
-                onInput={inputHandler}
-            />
-            <Input
-                id="address"
-                element="input"
-                type="text"
-                label="Description"
-                validators={[VALIDATOR_REQUIRE()]}
-                errorText="Please enter a valid address"
-                onInput={inputHandler}
-            />
-            <Button type="submit" disabled={!formState.isValid}>
-                ADD PLACE
-            </Button>
-        </form>
+        <React.Fragment>
+            <ErrorModal show={error} onClear={clearError} error={error} />
+            <form action="" className="place-form" onSubmit={formSubmitHandler}>
+                <Input
+                    id="title"
+                    element="input"
+                    type="text"
+                    label="Title"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a valid title"
+                    onInput={inputHandler}
+                />
+                <Input
+                    id="description"
+                    element="textarea"
+                    type="text"
+                    label="Description"
+                    validators={[VALIDATOR_MINLENGTH(5)]}
+                    errorText="Please enter a valid title (at least 5 characters)"
+                    onInput={inputHandler}
+                />
+                <Input
+                    id="address"
+                    element="input"
+                    type="text"
+                    label="Description"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a valid address"
+                    onInput={inputHandler}
+                />
+                <Button type="submit" disabled={!formState.isValid}>
+                    ADD PLACE
+                </Button>
+            </form>
+            {isLoading && <LoadingSpinner asOverlay />}
+        </React.Fragment>
     );
 };
 
