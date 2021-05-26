2be30da98fd92178d5cefc9fddccc92068133160
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-05-26 02:06:44 +0000

    include the image upload middle in sign up route

--- server/src/routes/users-routes.ts
+++ server/src/routes/users-routes.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { getUsers, logIn, signUp } from "../controllers/users-controllers";
 import { check } from "express-validator";
 
+import fileUpload from "../controllers/middleware/file-upload";
+
 const router = express.Router();
 
 router.get("/", getUsers);
 
 router.post(
     "/signup",
+    fileUpload.single("image"),
     [
         check("email").isEmail(),
         check("password").not().isEmpty().isLength({ min: 6 }),
