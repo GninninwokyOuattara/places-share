92029fcaa3ca43ff93decb7237a46e11fcc018bc
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-05-26 02:06:23 +0000

    image upload middleware

---
+++ server/src/controllers/middleware/file-upload.ts
@@ -0,0 +1,36 @@
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+
+const MIME_TYPE: { [key: string]: string } = {
+    "image/png": "png",
+    "image/jpg": "jpg",
+    "image/jpeg": "jpeg",
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "/upload/images");
+    },
+    filename: (req, file, cb) => {
+        const ext = `${uuidv4()}.${MIME_TYPE[file.mimetype]}`;
+        cb(null, ext);
+    },
+});
+
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 500000,
+    },
+    fileFilter: (req, file, cb) => {
+        const isValid = !!MIME_TYPE[file.mimetype];
+        let error = isValid ? null : new Error("Not a mime type");
+        if (error) {
+            cb(error);
+        } else {
+            cb(null, true);
+        }
+    },
+});
+
+export default upload;
