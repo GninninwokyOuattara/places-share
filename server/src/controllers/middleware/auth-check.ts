790a13c9a5b55f1c2bba0c55d70ae5a7d9e35575
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-05-28 00:09:01 +0000

    auth middleware

---
+++ server/src/controllers/middleware/auth-check.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import HttpError from "../../models/http-error";
+
+import { Request, Response, NextFunction } from "express";
+
+type RequestExtended = Request & {
+    userData?: { _id: string; email: string };
+};
+
+const authCheck = (req: RequestExtended, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization?.replace("BEARER ", "");
+        if (token) {
+            const decodedData = jwt.verify(token, "supersecret");
+            req.userData = {
+                _id: (decodedData as { user: string }).user,
+                email: (decodedData as { email: string }).email,
+            };
+            console.log(req.userData);
+            next();
+        } else {
+            throw new Error();
+        }
+    } catch (error) {
+        next(new HttpError("Authentification failed", 401));
+    }
+};
+
+export default authCheck;
